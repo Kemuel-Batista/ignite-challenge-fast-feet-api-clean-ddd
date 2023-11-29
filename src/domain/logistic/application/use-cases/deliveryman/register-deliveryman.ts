0b017f4458cff93710e4b4a29c81f26465aef09f
Kemuel Batista <kemuellima20@gmail.com>
2023-11-29 16:39:46 -0300

    feat: register deliveryman use case and test

---
+++ src/domain/logistic/application/use-cases/deliveryman/register-deliveryman.ts
@@ -0,0 +1,80 @@
+import { Deliveryman } from '@/domain/logistic/enterprise/entities/deliveryman'
+import { AdministratorsRepository } from '@/domain/logistic/application/repositories/administrators-repository'
+import { DeliverymansRepository } from '@/domain/logistic/application/repositories/deliverymans-repository'
+import { HashGenerator } from '@/domain/logistic/application/cryptography/hash-generator'
+import { DeliverymanAlreadyExistsError } from './errors/deliveryman-already-exists-error'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { Either, failure, success } from '@/core/either'
+
+interface RegisterDeliverymanUseCaseRequest {
+  adminId: string
+  name: string
+  lastname: string
+  email: string
+  cpf: string
+  phone: string
+  password: string
+}
+
+type RegisterDeliverymanUseCaseResponse = Either<
+  ResourceNotFoundError | DeliverymanAlreadyExistsError,
+  {
+    deliveryman: Deliveryman
+  }
+>
+
+export class RegisterDeliverymanUseCase {
+  constructor(
+    private administratorsRepository: AdministratorsRepository,
+    private deliverymansRepository: DeliverymansRepository,
+    private hashGenerator: HashGenerator,
+  ) {}
+
+  async execute({
+    adminId,
+    name,
+    lastname,
+    email,
+    cpf,
+    phone,
+    password,
+  }: RegisterDeliverymanUseCaseRequest): Promise<RegisterDeliverymanUseCaseResponse> {
+    const administratorExists =
+      await this.administratorsRepository.findById(adminId)
+
+    if (!administratorExists) {
+      return failure(new ResourceNotFoundError())
+    }
+
+    const deliverymanWithSameIdentifier =
+      await this.deliverymansRepository.findByIdentifier(cpf)
+
+    if (deliverymanWithSameIdentifier) {
+      return failure(new DeliverymanAlreadyExistsError(cpf))
+    }
+
+    const deliverymanWithSameEmail =
+      await this.deliverymansRepository.findByEmail(cpf)
+
+    if (deliverymanWithSameEmail) {
+      return failure(new DeliverymanAlreadyExistsError(email))
+    }
+
+    const hashedPassword = await this.hashGenerator.hash(password)
+
+    const deliveryman = Deliveryman.create({
+      name,
+      lastname,
+      email,
+      cpf,
+      phone,
+      password: hashedPassword,
+    })
+
+    await this.deliverymansRepository.create(deliveryman)
+
+    return success({
+      deliveryman,
+    })
+  }
+}
